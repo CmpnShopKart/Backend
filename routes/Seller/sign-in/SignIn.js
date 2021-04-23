d1174486a9056a5adbf7503a10a6fe25f3ead84e
Navin H Kachhela <kachhelanavin@gmail.com>
2021-04-23 16:25:07 +0530

    added seller Authentication in signin

--- routes/Seller/sign-in/SignIn.js
+++ routes/Seller/sign-in/SignIn.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const Seller = require('../../../models/Seller/Seller');
+const regSeller = require('../../../models/Seller/SellerRegistration');
 
 router.post('/signin',async (req,res) => {
     try{
         const seller = await Seller.findOne({'phoneNumber':req.body.phoneNumber}).exec();
-        if(seller){
-            if(seller.password===req.body.password){
-                res.json({...seller._doc,isValidCredentials:true})
+        const regseller = await  regSeller.findOne({'phoneNumber':req.body.phoneNumber}).exec();
+        
+        if(seller&&regSeller){
+           
+           
+            if(seller.password===req.body.password ){
+                res.json({...seller._doc,isValidCredentials:true,sellerRegistered:true});
             }else{
-                res.json({isValidCredentials:false})
+                    res.json({sellerRegistered:false,isValidCredentials:false});
             }
         }else{
-            res.json({isValidCredentials:false})
+            res.json({sellerRegistered:false,isValidCredentials:false});
+    
         }
     }catch(err){
+        console.log(err);
         res.json({message:err})
     }
 });
